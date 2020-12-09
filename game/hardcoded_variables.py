4fe1cd7debc05f29291c330d5a9848cb351931fa
Nivaldo Georg Junior <ngj.netrunner@gmail.com>
2020-12-09 12:27:20 -0300

    added hardcoded_variables.py

---
+++ game/hardcoded_variables.py
@@ -0,0 +1,24 @@
+# HARDCODED VARIABLES
+EXTINCT = 'Extinct'
+PLAYING = 'Playing'
+
+P1 = 'P1'
+P2 = 'P2'
+P3 = 'P3'
+
+ATK_SHIP_KEYWORDS = ['atk ships', 'as', 'attack ships', 'attack']
+MINING_SHIP_KEYWORDS = ['mining ships', 'ms', 'mining', 'miners']
+EXTRACTOR_SHIP_KEYWORDS = ['gas extractors', 'gas ships', 'gas']
+
+EXTRACTOR = 'Extractor'
+MINING = 'Mining'
+ATTACK = 'Attack'
+
+YES_GROUP = ['yes', 'si', 'sim', 'yeah', 'sure']
+NO_GROUP = ['no', "don't", 'not', 'no way']
+
+ATTACK_GROUP = ['attack', 'atk', 'deboxar', 'atacar']
+REFRESH_GROUP = ['ref', 'refresh', 'is my turn?']
+EXTRACT_GROUP = ['extract_gas', 'get gas', 'extract']
+MINE_GROUP = ['mine', 'gather-minerals']
+HANGAR_GROUP = ['hangar', 'buy', 'go to hangar']
